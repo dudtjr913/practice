7124f9dc502c2c8272e982b06409aa34314b6750
Cheon YeongSeok <dudtjr913@naver.com>
2020-12-26 15:26:58 +0900

    feat : Object 메소드 사용해보기

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import Dijkstra from './utils/Dijkstra.js';
+import { data } from "./utils/constant.js";
 
-const dijkstra = new Dijkstra();
+console.log(Object.keys(data)); // ["debug", "window", "image", "text"]
+console.log(Object.values(data.window)); // ["Sample Konfabulator Widget", "main_window", 500, 500]
+console.log(Object.entries(data.window));
+/*
+0: (2) ["title", "Sample Konfabulator Widget"]
+1: (2) ["name", "main_window"]
+2: (2) ["width", 500]
+3: (2) ["height", 500]
+*/
 
-dijkstra.addVertex('a');
-dijkstra.addVertex('c');
-dijkstra.addEdge('a', 'b', 5);
-dijkstra.addEdge('b', 'c', 1);
-dijkstra.addEdge('a', 'd', 7);
+// 숫자로만 구성된 요소 뽑아 배열로 만들기
+const extractNumbersArray = () => {
+  const numbersArray = [];
+  for (const key in data) {
+    const keyArray = Object.keys(data[key]);
+    keyArray.forEach((v) => {
+      if (typeof data[key][v] === "number") {
+        numbersArray.push(v);
+      }
+    });
+  }
+  console.log(numbersArray);
+};
 
-console.log(dijkstra.findShortestPath('a', 'c'));
+extractNumbersArray();
