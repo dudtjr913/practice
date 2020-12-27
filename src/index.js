95f8b593ec70133366f0eb238b07bf0bad4b0a91
Cheon YeongSeok <dudtjr913@naver.com>
2020-12-27 13:34:07 +0900

    feat : 'sk' type을 가진 사람만 출력

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { data } from "./utils/constant.js";
+import { dataOne, dataTwo } from "./utils/constant.js";
 
-console.log(Object.keys(data)); // ["debug", "window", "image", "text"]
-console.log(Object.values(data.window)); // ["Sample Konfabulator Widget", "main_window", 500, 500]
-console.log(Object.entries(data.window));
+console.log(Object.keys(dataOne)); // ["debug", "window", "image", "text"]
+console.log(Object.values(dataOne.window)); // ["Sample Konfabulator Widget", "main_window", 500, 500]
+console.log(Object.entries(dataOne.window));
 /*
 0: (2) ["title", "Sample Konfabulator Widget"]
 1: (2) ["name", "main_window"]
@@ -13,10 +13,10 @@ console.log(Object.entries(data.window));
 // 숫자로만 구성된 요소 뽑아 배열로 만들기
 const extractNumbersArray = () => {
   const numbersArray = [];
-  for (const key in data) {
-    const keyArray = Object.keys(data[key]);
+  for (const key in dataOne) {
+    const keyArray = Object.keys(dataOne[key]);
     keyArray.forEach((v) => {
-      if (typeof data[key][v] === "number") {
+      if (typeof dataOne[key][v] === "number") {
         numbersArray.push(v);
       }
     });
@@ -25,3 +25,41 @@ const extractNumbersArray = () => {
 };
 
 extractNumbersArray();
+
+const extractSkArray = () => {
+  const skArray = [];
+  dataTwo.forEach((one) => {
+    if (one.type === "sk") {
+      skArray.push(one.name);
+    }
+    one.childnode.forEach((two) => {
+      if (two.type === "sk") {
+        skArray.push(two.name);
+      }
+      two.childnode.forEach((three) => {
+        if (three.type === "sk") {
+          skArray.push(three.name);
+        }
+        three.childnode.forEach((four) => {
+          if (four.type === "sk") {
+            skArray.push(four.name);
+          }
+          four.childnode.forEach((five) => {
+            if (five.type === "sk") {
+              skArray.push(five.name);
+            }
+            five.childnode.forEach((six) => {
+              if (six.type === "sk") {
+                skArray.push(six.name);
+              }
+            });
+          });
+        });
+      });
+    });
+  });
+
+  console.log(skArray);
+};
+
+extractSkArray();
