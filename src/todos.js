4c22dd5837b6d39dfc7ee8c7b76a57baeaa321d5
unknown <dudtjr913@naver.com>
2020-12-29 17:52:04 +0900

    refactor : class로 변환 완료

--- src/todos.js
+++ src/todos.js
@@ -1,3 +1,174 @@
+class TodoManagement {
+  constructor() {
+    this.todosList = [];
+  }
+
+  addTodo(todoName, tags, status) {
+    if (this.isNameExist(todoName)) {
+      return alert('이름이 중복됩니다.');
+    }
+    if (!Array.isArray(tags) || !['todo', 'doing', 'done'].includes(status)) {
+      return alert(
+        'tags는 배열로, status는 todo, doing, done중에서 입력해주세요.',
+      );
+    }
+    const todoId = this.generateId();
+    const todoObj = {
+      name: todoName,
+      tags,
+      status,
+      id: todoId,
+    };
+    this.todosList.push(todoObj);
+
+    return todoObj;
+  }
+
+  removeTodo(todoName) {
+    const exNameIndex = this.todosList.findIndex(
+      (key) => key.name.trim() === todoName.trim(),
+    );
+    if (exNameIndex === -1) {
+      return alert('존재하지 않는 이름입니다.');
+    }
+
+    return this.todosList.splice(exNameIndex, 1);
+  }
+
+  isNameExist(todoName) {
+    const exName = this.todosList.find(
+      (key) => key.name.trim() === todoName.trim(),
+    );
+    if (exName) {
+      return true;
+    }
+
+    return false;
+  }
+
+  generateId() {
+    const todoId = Math.floor(Math.random() * 999999);
+    const exId = this.todosList.find((key) => key.id === todoId);
+    if (exId) {
+      return this.generateId();
+    }
+
+    return todoId;
+  }
+
+  todoStatusUpdate(todoName, status) {
+    const exTodo = this.todosList.find(
+      (key) => key.name.trim() === todoName.trim(),
+    );
+    if (!exTodo) {
+      return alert('존재하지 않는 이름입니다.');
+    }
+    if (
+      exTodo.status === status ||
+      !['todo', 'doing', 'done'].includes(status)
+    ) {
+      return alert(
+        '상태가 기존과 동일하거나 todo, doing, done 중에 입력되지 않았습니다.',
+      );
+    }
+
+    return (exTodo.status = status);
+  }
+
+  addTags(todoName, tagName) {
+    const exTodo = this.todosList.find(
+      (key) => key.name.trim() === todoName.trim(),
+    );
+    if (!exTodo) {
+      return alert('존재하지 않는 이름입니다.');
+    }
+    if (typeof tagName === 'string') {
+      return exTodo.tags.push(tagName);
+    }
+    if (Array.isArray(tagName)) {
+      return (exTodo.tags = [...exTodo.tags, ...tagName]);
+    }
+
+    return alert('문자열이나 배열로 입력해주세요.');
+  }
+
+  removeTags(todoName, tagName) {
+    const exTodo = this.todosList.find(
+      (key) => key.name.trim() === todoName.trim(),
+    );
+    if (!exTodo) {
+      return alert('존재하지 않는 이름입니다.');
+    }
+    const exTagIndex = exTodo.tags.findIndex((tag) => tag === tagName);
+    if (exTagIndex === -1) {
+      return alert('존재하지 않는 태그입니다.');
+    }
+
+    return exTodo.tags.splice(exTagIndex, 1);
+  }
+
+  changeTodoName(todoName, changeName) {
+    if (this.isNameExist(changeName)) {
+      return alert('변경하려는 이름이 중복됩니다.');
+    }
+    const exTodo = this.todosList.find(
+      (key) => key.name.trim() === todoName.trim(),
+    );
+    if (!exTodo) {
+      return alert('존재하지 않는 이름입니다.');
+    }
+
+    return (exTodo.name = changeName);
+  }
+}
+
+class Todo extends TodoManagement {
+  show(status) {
+    if (!['all', 'todo', 'doing', 'done'].includes(status)) {
+      return alert('all, todo, doing, done중에서 입력해주세요.');
+    }
+    if (status === 'all') {
+      return this.showAll();
+    }
+
+    return this.showSelectedStatus(status);
+  }
+
+  showAll() {
+    const allList = {};
+    this.todosList.forEach((key) => {
+      if (allList[key.status]) {
+        return allList[key.status]++;
+      }
+      return (allList[key.status] = 1);
+    });
+
+    const convertedResult = Object.entries(allList).map((v) => {
+      v[v.length - 1] += '개';
+      return v.join(':');
+    });
+
+    return `현재상태 : ${convertedResult.join(', ')}`;
+  }
+
+  showSelectedStatus(status) {
+    const statusList = [];
+    this.todosList.forEach((key) => {
+      if (key.status === status) {
+        return statusList.push(key.name);
+      }
+    });
+
+    return `${status}리스트:총${statusList.length}건, ${statusList.join(', ')}`;
+  }
+}
+
+const todo = new Todo();
+todo.addTodo('그림 그리기', ['paint'], 'todo');
+todo.removeTodo('그림 그리기');
+todo.addTags('그림 그리기', '어허허');
+console.log(todo.show('done'));
+
 /* 
 // 할 일 관리 클래스
 - 할 일 입력 : 할 일을 추가할 수 있는데, name, tags, status를 입력받는다. -> 이름 중복 확인 -> id 부여 -> 할 일 추가
@@ -37,61 +208,6 @@ f 할 일 입력 addTodo(todoName, tags, status){
 }
 */
 
-const todosList = [
-  {
-    'name': '자바스크립트 공부하기',
-    'tags': ['programming', 'javascript'],
-    'status': 'todo',
-    'id': 12123123,
-  },
-  {
-    'name': ' 그림 그리기',
-    'tags': ['picture', 'favorite'],
-    'status': 'doing',
-    'id': 312323,
-  },
-];
-
-const addTodo = (todoName, tags, status) => {
-  if (isNameExist(todoName)) {
-    return alert('이름이 중복됩니다.');
-  }
-  if (!Array.isArray(tags) || !['todo', 'doing', 'done'].includes(status)) {
-    return alert(
-      'tags는 배열로, status는 todo, doing, done중에서 입력해주세요.',
-    );
-  }
-  const todoId = generateId();
-  const todoObj = {
-    name: todoName,
-    tags,
-    status,
-    id: todoId,
-  };
-  todosList.push(todoObj);
-
-  return todoObj;
-};
-
-const isNameExist = (todoName) => {
-  const exName = todosList.find((key) => key.name.trim() === todoName.trim());
-  if (exName) {
-    return true;
-  }
-
-  return false;
-};
-
-const generateId = () => {
-  const todoId = Math.floor(Math.random() * 999999);
-  const exId = todosList.find((key) => key.id === todoId);
-  if (exId) {
-    return generateId();
-  }
-
-  return todoId;
-};
-
 /*
 // 할 일 삭제
 f 할 일 삭제 removeTodo(todoName){
@@ -106,17 +222,6 @@ f 할 일 삭제 removeTodo(todoName){
    todosList에서 splice로 삭제
 }
 */
-
-const removeTodo = (todoName) => {
-  const exNameIndex = todosList.findIndex(
-    (key) => key.name.trim() === todoName.trim(),
-  );
-  if (exNameIndex === -1) {
-    return alert('존재하지 않는 이름입니다.');
-  }
-
-  return todosList.splice(exNameIndex, 1);
-};
 
 /*
 // 상태 업데이트
@@ -136,19 +241,6 @@ f 상태 업데이트 todoStatusUpdate(todoName, status){
     변경할 할 일.status = status
 }
 */
-const todoStatusUpdate = (todoName, status) => {
-  const exTodo = todosList.find((key) => key.name.trim() === todoName.trim());
-  if (!exTodo) {
-    return alert('존재하지 않는 이름입니다.');
-  }
-  if (exTodo.status === status || !['todo', 'doing', 'done'].includes(status)) {
-    return alert(
-      '상태가 기존과 동일하거나 todo, doing, done 중에 입력되지 않았습니다.',
-    );
-  }
-
-  return (exTodo.status = status);
-};
 
 /*
 // 태그 추가
@@ -168,20 +260,6 @@ f 태그 추가 addTags(todoName, tagName){
     }
 }
 */
-const addTags = (todoName, tagName) => {
-  const exTodo = todosList.find((key) => key.name.trim() === todoName.trim());
-  if (!exTodo) {
-    return alert('존재하지 않는 이름입니다.');
-  }
-  if (typeof tagName === 'string') {
-    return exTodo.tags.push(tagName);
-  }
-  if (Array.isArray(tagName)) {
-    return (exTodo.tags = [...exTodo.tags, ...tagName]);
-  }
-
-  return alert('문자열이나 배열로 입력해주세요.');
-};
 
 /*
 // 태그 삭제
@@ -202,18 +280,6 @@ f 태그 삭제 removeTags(todoName, tagName){
     return 할 일.tags.splice를 사용해 태그 삭제
 }
 */
-const removeTags = (todoName, tagName) => {
-  const exTodo = todosList.find((key) => key.name.trim() === todoName.trim());
-  if (!exTodo) {
-    return alert('존재하지 않는 이름입니다.');
-  }
-  const exTagIndex = exTodo.tags.findIndex((tag) => tag === tagName);
-  if (exTagIndex === -1) {
-    return alert('존재하지 않는 태그입니다.');
-  }
-
-  return exTodo.tags.splice(exTagIndex, 1);
-};
 
 /*
 // 이름 변경
@@ -233,17 +299,6 @@ f 이름 변경 changeTodoName(todoName, changeName){
     return 할 일.name = changeName
 }
 */
-const changeTodoName = (todoName, changeName) => {
-  if (isNameExist(changeName)) {
-    return alert('변경하려는 이름이 중복됩니다.');
-  }
-  const exTodo = todosList.find((key) => key.name.trim() === todoName.trim());
-  if (!exTodo) {
-    return alert('존재하지 않는 이름입니다.');
-  }
-
-  return (exTodo.name = changeName);
-};
 
 /*
 // 할 일 보여주기 클래스
@@ -258,16 +313,6 @@ f 상태 보여주기 show(status){
   return 상태에 맞는 할 일 보여주기 메소드 호출(status)
 }
 */
-const show = (status) => {
-  if (!['all', 'todo', 'doing', 'done'].includes(status)) {
-    return alert('all, todo, doing, done중에서 입력해주세요.');
-  }
-  if (status === 'all') {
-    return showAll();
-  }
-
-  return showSelectedStatus(status);
-};
 
 /*
 // 모든 상태의 할 일 보여주기
@@ -288,22 +333,6 @@ f 모든 상태의 할 일 보여주기 showAll(){
   Object.entries를 사용해 array로 변경 후 return
 }
 */
-const showAll = () => {
-  const allList = {};
-  todosList.forEach((key) => {
-    if (allList[key.status]) {
-      return allList[key.status]++;
-    }
-    return (allList[key.status] = 1);
-  });
-
-  const convertedResult = Object.entries(allList).map((v) => {
-    v[v.length - 1] += '개';
-    return v.join(':');
-  });
-
-  return `현재상태 : ${convertedResult.join(', ')}`;
-};
 
 /*
 // 상태에 맞는 할 일 보여주기
@@ -316,13 +345,3 @@ f 상태에 맞는 할 일 보여주기 showSelectedStatus(status){
   상태에 맞는 할 일 목록에서 총 길이와 join메소드를 사용해 보여주기 return
 }
 */
-const showSelectedStatus = (status) => {
-  const statusList = [];
-  todosList.forEach((key) => {
-    if (key.status === status) {
-      return statusList.push(key.name);
-    }
-  });
-
-  return `todo리스트 : 총${statusList.length}건 : ${statusList.join(', ')}`;
-};
