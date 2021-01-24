f8de3da11e5b5751d9a58ac45103827033e1b603
unknown <dudtjr913@naver.com>
2021-01-24 16:59:21 +0900

    update : 메소드 추가 기능 구현 및 freeze 사용

--- src/class.js
+++ src/class.js
@@ -1,23 +1,3 @@
-/* 
-function Bridge() {}
-
-function Employee(name, age, job) {
-  this.name = name || '이름 모름';
-  this.age = age || '나이 모름';
-  this.job = job || '직업 모름';
-}
-
-Bridge.prototype = Person.prototype;
-
-console.log(Bridge.prototype);
-Employee.prototype = new Bridge();
-Employee.prototype.getJob = function () {
-  return this.job;
-};
-
-const employee = new Employee('영석', 27, '프론트엔드 개발자');
-console.log(Employee.prototype); */
-
 function Person(name, age) {
   this.name = name || '이름 모름';
   this.age = age || '나이 모름';
@@ -33,20 +13,45 @@ Person.prototype.getAge = function () {
 
 const extendsClass = (() => {
   function Bridge() {}
-  return (SuperClass, SubClass) => {
+  return (SuperClass, SubClass, subMethod) => {
     Bridge.prototype = SuperClass.prototype;
     SubClass.prototype = new Bridge();
     SubClass.prototype.constructor = SubClass;
+
+    if (subMethod) {
+      subMethod.forEach((method) => {
+        for (const key in method) {
+          SubClass.prototype[key] = method[key];
+        }
+      });
+    }
+    Object.freeze(SubClass.prototype);
 
     return SubClass;
   };
 })();
 
-const Employee = extendsClass(Person, function (name, age, job) {
-  this.name = name || '이름 모름';
-  this.age = age || '나이 모름';
-  this.job = job || '직업 모름';
-});
+const Employee = extendsClass(
+  Person,
+  function (name, age, job) {
+    this.name = name || '이름 모름';
+    this.age = age || '나이 모름';
+    this.job = job || '직업 모름';
+  },
+  [
+    {
+      getJob: function () {
+        return this.job;
+      },
+    },
+    {
+      getInfo: function () {
+        return {name: this.name, age: this.age, job: this.job};
+      },
+    },
+  ],
+);
 
 const yeong = new Employee('영석', 27, '프론트엔드 개발자');
-console.log(yeong);
+
+console.log(yeong.getInfo());
