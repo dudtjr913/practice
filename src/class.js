d28cbcfe298700746874921dddaba986e1552331
unknown <dudtjr913@naver.com>
2021-01-24 16:43:51 +0900

    update : class 확장 함수 생성

--- src/class.js
+++ src/class.js
@@ -1,14 +1,26 @@
-function Person(name, age) {
-  this.name = name || '이름 모름';
-  this.age = age || '나이 모름';
-}
-
+/* 
 function Bridge() {}
 
 function Employee(name, age, job) {
   this.name = name || '이름 모름';
   this.age = age || '나이 모름';
   this.job = job || '직업 모름';
+}
+
+Bridge.prototype = Person.prototype;
+
+console.log(Bridge.prototype);
+Employee.prototype = new Bridge();
+Employee.prototype.getJob = function () {
+  return this.job;
+};
+
+const employee = new Employee('영석', 27, '프론트엔드 개발자');
+console.log(Employee.prototype); */
+
+function Person(name, age) {
+  this.name = name || '이름 모름';
+  this.age = age || '나이 모름';
 }
 
 Person.prototype.getName = function () {
@@ -19,13 +31,22 @@ Person.prototype.getAge = function () {
   return this.age;
 };
 
-Bridge.prototype = Person.prototype;
+const extendsClass = (() => {
+  function Bridge() {}
+  return (SuperClass, SubClass) => {
+    Bridge.prototype = SuperClass.prototype;
+    SubClass.prototype = new Bridge();
+    SubClass.prototype.constructor = SubClass;
 
-Employee.prototype = new Bridge();
-Employee.prototype.constructor = Employee;
-Employee.prototype.getJob = function () {
-  return this.job;
-};
+    return SubClass;
+  };
+})();
 
-const employee = new Employee('영석', 27, '프론트엔드 개발자');
-console.log(employee);
+const Employee = extendsClass(Person, function (name, age, job) {
+  this.name = name || '이름 모름';
+  this.age = age || '나이 모름';
+  this.job = job || '직업 모름';
+});
+
+const yeong = new Employee('영석', 27, '프론트엔드 개발자');
+console.log(yeong);
