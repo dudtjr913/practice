f334582a84e883ea6af9005e90d72a956decd268
unknown <dudtjr913@naver.com>
2021-01-26 12:32:39 +0900

    update : 제너레이터 연습

--- src/functional/index.js
+++ src/functional/index.js
@@ -1,12 +1,17 @@
 const log = console.log;
 
-function* odds(number) {
-  for (let i = 0; i < number; i++) {
-    if (i % 2) yield i;
+function* infinity(startNumber = 0) {
+  while (true) yield startNumber++;
+}
+
+function* odds(startNumber, finishNumber) {
+  for (const number of infinity(startNumber)) {
+    if (number % 2) yield number;
+    if (number === finishNumber) return;
   }
 }
 
-const oddsIterator = odds(10);
+const oddsIterator = odds(1, 7);
 log(oddsIterator.next());
 log(oddsIterator.next());
 log(oddsIterator.next());
