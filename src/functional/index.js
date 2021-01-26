c44d50987c9065ffe16c8fcbc4c791d226ae54a8
unknown <dudtjr913@naver.com>
2021-01-26 12:59:38 +0900

    update : 제너레이터 연습 - 쪼개기

--- src/functional/index.js
+++ src/functional/index.js
@@ -4,16 +4,20 @@ function* infinity(startNumber = 0) {
   while (true) yield startNumber++;
 }
 
-function* odds(startNumber, finishNumber) {
-  for (const number of infinity(startNumber)) {
-    if (number % 2) yield number;
+function* limit(iterable, finishNumber) {
+  for (const number of iterable) {
+    yield number;
     if (number === finishNumber) return;
   }
 }
 
+function* odds(startNumber, finishNumber) {
+  for (const number of limit(infinity(startNumber), finishNumber)) {
+    if (number % 2) yield number;
+  }
+}
+
 const oddsIterator = odds(1, 7);
-log(oddsIterator.next());
-log(oddsIterator.next());
 log(oddsIterator.next());
 log(oddsIterator.next());
 log(oddsIterator.next());
