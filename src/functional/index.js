3d8a5a1761485878dbb1599b220847e9a5b5b36e
unknown <dudtjr913@naver.com>
2021-01-26 13:54:13 +0900

    feat : 제너레이터 함수로 중복되는 수 없이 추출하기

--- src/functional/index.js
+++ src/functional/index.js
@@ -1,24 +1,39 @@
 const log = console.log;
 
-function* infinity(startNumber = 0) {
-  while (true) yield startNumber++;
-}
+const getAnswerNumber = (numberArray, count) => {
+  for (const answerNumber of createAnswerNumber(numberArray, count)) {
+    if (answerNumber.length === count) {
+      return answerNumber;
+    }
+  }
+};
 
-function* limit(iterable, finishNumber) {
-  for (const number of iterable) {
-    yield number;
-    if (number === finishNumber) return;
+const isRightArg = (numberArray, count) => {
+  if (new Set(numberArray).size !== numberArray.length) {
+    return console.error('array에 중복되는 수가 있습니다.');
+  }
+  if (numberArray.length < count) {
+    return console.error('count가 array의 길이보다 작아야 합니다.');
+  }
+
+  return true;
+};
+
+function* createAnswerNumber(numberArray, count) {
+  if (!isRightArg(numberArray, count)) return;
+
+  const answer = [];
+
+  while (answer.length !== count) {
+    const randomIndex = Math.floor(Math.random() * numberArray.length);
+    const randomNumber = numberArray[randomIndex];
+
+    if (!answer.includes(randomNumber)) {
+      answer.push(randomNumber);
+      yield answer;
+    }
   }
 }
 
-function* odds(startNumber, finishNumber) {
-  for (const number of limit(infinity(startNumber), finishNumber)) {
-    if (number % 2) yield number;
-  }
-}
-
-const oddsIterator = odds(1, 7);
-log(oddsIterator.next());
-log(oddsIterator.next());
-log(oddsIterator.next());
-log(oddsIterator.next());
+const answer = getAnswerNumber([1, 2, 3, 4, 5], 5);
+console.log(answer);
