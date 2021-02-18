fb998bee13e0506e9011aae760f8954e099ccccd
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-18 21:47:04 +0900

    refactor : usd 함수 선언 바꾸기

--- src/refactoring/statement.js
+++ src/refactoring/statement.js
@@ -9,13 +9,13 @@ const statement = (invoice, plays) => {
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
     // 청구 내역 출력
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
@@ -60,7 +60,7 @@ const statement = (invoice, plays) => {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 };
 
