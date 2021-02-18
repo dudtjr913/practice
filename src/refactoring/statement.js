428beb7ea3c16aff1991ed8fbd0b361a3fce5134
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-19 00:42:50 +0900

    refactor : 함수 옮기기

--- src/refactoring/statement.js
+++ src/refactoring/statement.js
@@ -9,27 +9,14 @@ const statement = (invoice, plays) => {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(aPerformance)
+    result.play = playFor(result);
+    result.amount = amountFor(result)
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-};
-
-const renderPlainText = (data) => {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
 
   function amountFor(aPerformance) {
     let result = 0;
@@ -48,11 +35,25 @@ const renderPlainText = (data) => {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
 
     return result;
   }
+};
+
+const renderPlainText = (data) => {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    }석)\n`;
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
@@ -83,7 +84,7 @@ const renderPlainText = (data) => {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount
     }
 
     return result;
