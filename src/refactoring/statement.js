52992cf7279f5fe8d71e7482c7ad811e4e804788
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-18 21:36:15 +0900

    refactor : 변수 인라인하기

--- src/refactoring/statement.js
+++ src/refactoring/statement.js
@@ -12,16 +12,15 @@ const statement = (invoice, plays) => {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf)
-    const thisAmount = amountFor(perf, play);
+    const thisAmount = amountFor(perf);
 
     // 포인트 적립
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트 제공
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역 출력
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
@@ -31,9 +30,9 @@ const statement = (invoice, plays) => {
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case 'tragedy':
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -48,7 +47,7 @@ const statement = (invoice, plays) => {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
 
     return result;
