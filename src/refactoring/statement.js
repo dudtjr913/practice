c2aa84c8270f93a1e018c5efc64b2115638b34bb
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-19 00:35:48 +0900

    refactor : 함수 옮기기

--- src/refactoring/statement.js
+++ src/refactoring/statement.js
@@ -9,15 +9,20 @@ const statement = (invoice, plays) => {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
+    result.play = playFor(aPerformance)
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 };
 
-const renderPlainText = (data, plays) => {
+const renderPlainText = (data) => {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
   }
@@ -28,7 +33,7 @@ const renderPlainText = (data, plays) => {
 
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case 'tragedy':
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -49,14 +54,10 @@ const renderPlainText = (data, plays) => {
     return result;
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === playFor(aPerformance).type)
+    if ('comedy' === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
 
     return result;
