f67a7640456888c95f41d20a70d6289ab8845eb5
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-18 21:49:09 +0900

    refactor : 문장 슬라이드하기

--- src/refactoring/statement.js
+++ src/refactoring/statement.js
@@ -3,9 +3,9 @@ import plays from '../json/plays.js';
 
 const statement = (invoice, plays) => {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
+  
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
