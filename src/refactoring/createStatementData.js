98a6c1513d9511a5cb1272b6347c0b95eca3bf7c
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-20 21:01:07 +0900

    feat : ComedyCalculator class를 만들어 다형성 지원

--- src/refactoring/createStatementData.js
+++ src/refactoring/createStatementData.js
@@ -93,3 +93,15 @@ class TragedyCalculator extends PerformanceCalculator {
     return result;
   }
 }
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+
+    return result;
+  }
+}
