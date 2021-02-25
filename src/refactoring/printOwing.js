7225e3c8761607d0a0faea32154b3c1953b46e21
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-25 20:21:16 +0900

    refactor : 함수 분리하기 printBanner

--- src/refactoring/printOwing.js
+++ src/refactoring/printOwing.js
@@ -14,9 +14,7 @@ const invoice = {
 const printOwing = (invoice) => {
   let outstanding = 0;
 
-  console.log('*****************');
-  console.log('**** 고객 채무 ****');
-  console.log('*****************');
+  printBanner();
 
   for (const o of invoice.orders) {
     outstanding += o.amount;
@@ -31,6 +29,12 @@ const printOwing = (invoice) => {
   console.log(`고객명: ${invoice.customer}`);
   console.log(`채무액: ${outstanding}`);
   console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+};
+
+const printBanner = () => {
+  console.log('*****************');
+  console.log('**** 고객 채무 ****');
+  console.log('*****************');
 };
 
 printOwing(invoice);
