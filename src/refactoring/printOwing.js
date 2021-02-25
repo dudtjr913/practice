835479f2683effb69451a7dff959a13d2674f9a4
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-25 20:12:26 +0900

    feat : 함수 추출하기 연습하기 위한 기능 추가

---
+++ src/refactoring/printOwing.js
@@ -0,0 +1,21 @@
+const printOwing = (invoice) => {
+  let outstanding = 0;
+
+  console.log('*****************');
+  console.log('**** 고객 채무 ****');
+  console.log('*****************');
+
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  invoice.dueDate = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    new Date().getDate(),
+  );
+
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+};
