49ca456f43d8cc5074c708327ce5f0b31492bae0
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-25 20:20:29 +0900

    chore : invoice 생성

--- src/refactoring/printOwing.js
+++ src/refactoring/printOwing.js
@@ -1,3 +1,16 @@
+const invoice = {
+  customer: 'ditto',
+  orders: [
+    {
+      amount: 3000,
+    },
+    {
+      amount: 5000,
+    },
+  ],
+  dueDate: null,
+};
+
 const printOwing = (invoice) => {
   let outstanding = 0;
 
@@ -19,3 +32,5 @@ const printOwing = (invoice) => {
   console.log(`채무액: ${outstanding}`);
   console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 };
+
+printOwing(invoice);
