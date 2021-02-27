7238096ef75b23fb28ec55d5db8b901ac8fee021
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-27 22:00:56 +0900

    chore : 함수 인라인하기 연습

--- src/refactoring/inline.js
+++ src/refactoring/inline.js
@@ -7,3 +7,21 @@ const rating = (aDriver) => {
 };
 
 console.log(rating(aDriver));
+
+const aCustomer = {
+  name: 'ditto',
+  location: 'seoul',
+};
+
+const reportLines = (aCustomer) => {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+};
+
+const gatherCustomerData = (out, aCustomer) => {
+  out.push(['name', aCustomer.name]);
+  out.push(['location', aCustomer.location]);
+};
+
+console.log(reportLines(aCustomer));
