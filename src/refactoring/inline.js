9c312dec93b408958f66f0be309160c98f72de5c
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-27 21:59:02 +0900

    refactor : 함수 인라인하기

--- src/refactoring/inline.js
+++ src/refactoring/inline.js
@@ -3,9 +3,7 @@ const aDriver = {
 };
 
 const rating = (aDriver) => {
-  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
-};
-
-const moreThanFiveLateDeliveries = (aDriver) => {
   return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 };
+
+console.log(rating(aDriver));
