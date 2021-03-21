8dfd9476a7dbe8be6f32438c4b159214136161f8
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-03-21 23:12:47 +0900

    feat: webpack 설정

--- src/webpack.js
+++ src/webpack.js
@@ -1,1 +1,9 @@
-console.log('aa');
+class Webpack {
+  test() {
+    console.log('test');
+  }
+}
+
+const webpack = new Webpack();
+webpack.test();
+console.log('aaddszzxx');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: { main: './src/webpack.js' },
+  entry: './src/webpack.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+    }),
+  ],
 };
