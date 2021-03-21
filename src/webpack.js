3eddfbe38e6ebb62ce42f69406fe11e02e37e896
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-03-22 00:23:38 +0900

    feat: 웹팩 연습

--- src/webpack.js
+++ src/webpack.js
@@ -1,3 +1,4 @@
+import '../style.css';
 class Webpack {
   test() {
     console.log('test');
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/webpack.js',
+  entry: './src/webpack.js', // 없으면 자동으로 index.js를 찾는 듯
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'), // 없으면 자동으로 /dist로 지정해주는 듯
+    filename: 'main.js', // 없으면 자동으로 main.js로 해주는 듯
+    publicPath: '/dist/', // url의 path를 설정
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+        },
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
       template: './index.html',
     }),
   ],
