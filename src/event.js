020c0e6d248ce8cfbd899bd9bb9679088f2777e7
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-03-28 22:48:34 +0900

    feat: keyup이벤트 한글 입력 후 엔터 누르면 2번 호출 막기

--- src/event.js
+++ src/event.js
@@ -1,9 +1,17 @@
-document.querySelector('#fruits').addEventListener(
-  'click',
-  (e) => {
-    console.log(e.eventPhase);
-    console.log(e.target);
-    console.log(e.currentTarget);
-  },
-  true,
-);
+document.querySelector('#fruits').addEventListener('click', (e) => {
+  console.log(e);
+});
+
+document.querySelector('#apple').addEventListener('click', (e) => {
+  e.stopImmediatePropagation();
+  console.log(e);
+});
+
+document.querySelector('#apple').addEventListener('click', (e) => {
+  console.log(e);
+});
+
+document.querySelector('#input').addEventListener('keyup', (e) => {
+  if (e.keyCode === 229) return;
+  console.log(e.target.value);
+});
