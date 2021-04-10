9007430f64ce3d3269cd59b6285a6a8b23845168
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-04-10 19:47:40 +0900

    feat: Promise, async await

--- src/Promise.js
+++ src/Promise.js
@@ -1,3 +1,16 @@
+const getPromiseResult = (url) => {
+  const result = fetch(url);
+  return result
+    .then((a) => {
+      if (a.ok) {
+        return a.text();
+      } else {
+        throw new Error(a);
+      }
+    })
+    .then(console.log);
+};
+
 const practicePromise = (url) => {
   return new Promise((resolve, reject) => {
     document.querySelector('#loading').classList.remove('hide');
@@ -15,4 +28,45 @@ const practicePromise = (url) => {
   });
 };
 
-practicePromise();
+/* const func = async () => {
+  console.log(await getPromiseResult('http://jsonplaceholder.typicode.com'));
+
+  console.log('aa');
+}; */
+
+/* func(); */
+
+const asyncAwait = async () => {
+  const a = await 1;
+  return a;
+};
+
+asyncAwait().then(console.log);
+
+const test = async () => {
+  const first = await new Promise((resolve) => setTimeout(() => resolve(1), 3000));
+  const second = await new Promise((resolve) => setTimeout(() => resolve(2), 2000));
+  const third = await new Promise((resolve) => setTimeout(() => resolve(3), 1000));
+
+  console.log(first, second, third);
+};
+
+/* const promiseAll = async () => {
+  const res = await Promise.all([
+    new Promise((resolve) => setTimeout(() => resolve(1), 3000)),
+    new Promise((resolve) => setTimeout(() => resolve(2), 2000)),
+    new Promise((resolve) => setTimeout(() => resolve(3), 1000)),
+  ]);
+
+  console.log(res);
+  console.log('aa');
+};
+ */
+test();
+
+/* let i = 0;
+setTimeout(() => console.log(i++, 'A'), 0);
+requestAnimationFrame(() => console.log(i++, 'B'));
+Promise.resolve().then(() => console.log(i++, 'C'));
+console.log(i++, ' D');
+ */
