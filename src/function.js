7d7c4eb18000c99d46aae9e0f3a20da8c32d13fc
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-04-18 22:15:11 +0900

    feat: curry

--- src/function.js
+++ src/function.js
@@ -6,3 +6,21 @@ console.log(`${num} ${func2.name || 'Anonymous'}: ${func2()}`); // 1 Anonymous: 
 num = 2;
 console.log(`${num} ${func1.name || 'Anonymous'}: ${func1()}`); // 2 func2 : 2
 console.log(`${num} ${func2.name || 'Anonymous'}: ${func2()}`); // 2 Anonymous: 1
+
+const curry = (binaryFn) => (firstArg) => (secondArg) => binaryFn(firstArg, secondArg);
+
+const convertCurry = (binaryFn) => {
+  return function (firstArg) {
+    return function (secondArg) {
+      return binaryFn(firstArg, secondArg);
+    };
+  };
+};
+
+const genericTable = (x, y) => x * y;
+
+const tableOf2 = curry(genericTable)(2);
+const tableOf22 = convertCurry(genericTable)(2);
+
+tableOf2(4);
+tableOf22(4);
