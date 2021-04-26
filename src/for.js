ca803b9134edd9eb0c6487a3453be9eef85dfa17
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-04-26 20:51:55 +0900

    feat: for문의 scope

--- src/for.js
+++ src/for.js
@@ -4,17 +4,16 @@ const crews = [
 ];
 
 const findCrew = (crewId, callback) => {
-  setTimeout(() => {
-    const crewName = crews.find((crew) => crew.id === crewId).name;
-    callback(crewName);
-  }, 10);
+  const crewName = crews.find((crew) => crew.id === crewId).name;
+  callback(crewName);
 };
 
-for (var crew of crews) {
-  findCrew(crew.id, (name) => {
-    console.log(crew, name);
-  });
+for (var index = 0; index < crews.length - 1; ++index) {
+  console.log(index);
 }
+
+findCrew(crews[index].id, (v) => console.log(v));
+findCrew(crews[index].id, (v) => console.log(v));
 
 // {id: 2, name: "cheffe"} "ditto"
 // {id: 2, name: "cheffe"} "cheffe"
